bddbeb3914dd087b9fcd100a70e3314b9007a801
Captain <71038018+karimi65@users.noreply.github.com>
2021-01-17 20:51:58 -0600

    add Questins

--- index.js
+++ index.js
@@ -1,3 +1,71 @@
 const fs = require('fs')
 const inquirer = require('inquirer')
 const generateMarkdown = require('./generateMarkdown.js')
+
+// array of questions for user
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'link',
+        message: 'Add a link to your project webpage',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'write a brief description about your project',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use. Include screenshots as needed.',
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators, if any, with links to their GitHub profiles.',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Write tests for your application.',
+    },
+    {
+        type: 'lists',
+        name: 'license',
+        message: 'What license did you use?',
+        choices: ['Artistic license 2.0', 'MIT', 'ISC', 'The Unlicense']
+    },
+    {
+        type: 'input',
+        name: 'GitHub',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: 'What is your Email address?',
+    },
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
